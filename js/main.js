451af7d7eba31ba3642c571a99c784220d4af52b
Ivan <31728037+Navi-coM@users.noreply.github.com>
2020-03-22 16:47:53 +0200

    Develop main block

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(".main-slider").slick({
+  dots: true,
+  autoplay: true,
+  infinity: true,
+  autoplaySpeed: 5000,
+  speed: 1700
+});
 
+$(".men-slider").slick({
+  dots: true,
+  autoplay: true,
+  infinity: true,
+  autoplaySpeed: 50000,
+  speed: 1700,
+  slidesToShow: 4
+});
+
+$("main-slider-women").slick({
+  dots: true,
+  autoplay: true,
+  infinity: true,
+  autoplaySpeed: 5000,
+  speed: 1700,
+  slidesToShow: 4
+});
