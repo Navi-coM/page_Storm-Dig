ce4378d785dba0a922d4c1706ab3609d6fbe2bb7
Ivan <31728037+Navi-coM@users.noreply.github.com>
2020-03-23 14:31:22 +0200

    Finish dev footer

--- js/main.js
+++ js/main.js
@@ -6,20 +6,25 @@ $(".main-slider").slick({
   speed: 1700
 });
 
-$(".men-slider").slick({
+$(".men-slider, .women-slider").slick({
   dots: true,
   autoplay: true,
   infinity: true,
-  autoplaySpeed: 50000,
-  speed: 1700,
-  slidesToShow: 4
-});
-
-$(".women-slider").slick({
-  dots: true,
-  autoplay: true,
-  infinity: true,
-  autoplaySpeed: 5000,
-  speed: 1700,
-  slidesToShow: 4
+  autoplaySpeed: 7000,
+  speed: 1600,
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]
 });
